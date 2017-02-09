f42057be03188b3603b9504d07fd7a4efb651e97
Djilani Kebaili <djilani.kebaili@epfl.ch>
2017-02-09 16:55:20 +0100

    Android app

---
+++ sample_code/android/OpenFoodAPIDemoAndroid/app/src/main/java/com/salathelab/openfoodapidemo/MainActivity.java
@@ -0,0 +1,136 @@
+package com.salathelab.openfoodapidemo;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.BackgroundColorSpan;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.RelativeSizeSpan;
+import android.text.style.StyleSpan;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import com.salathelab.openfoodapidemo.async.ProductsDownloader;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText txtKey;
+    private Button btnToggleDownload;
+    private TextView txtResult;
+    private TextView lblStatus;
+    private RelativeLayout lytMain;
+    private ScrollView scrlTxtResult;
+    private CheckBox chkScrollContent;
+
+    boolean debugOutput = true;
+    boolean downloaderActive;
+    ProductsDownloader productsDownloader;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        lytMain = (RelativeLayout)findViewById(R.id.activity_main);
+        txtKey = (EditText)findViewById(R.id.txt_key);
+        btnToggleDownload = (Button)findViewById(R.id.btn_toggle_download);
+        scrlTxtResult = (ScrollView)findViewById(R.id.scroller_for_txt_result);
+        chkScrollContent = (CheckBox)findViewById(R.id.chk_scroll_content);
+        txtResult = (TextView)findViewById(R.id.txt_result);
+        txtResult = (TextView)findViewById(R.id.txt_result);
+        lblStatus = (TextView)findViewById(R.id.lbl_status);
+
+        btnToggleDownload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clearFocus();
+                if (downloaderActive) {
+                    stopDownload();
+                } else {
+                    startDownload();
+                }
+                toggleDownloaderActive();
+            }
+        });
+    }
+
+    private void startDownload() {
+        txtResult.setText("");
+        productsDownloader = new ProductsDownloader();
+        ProductsDownloader.DownloaderParamsStart params = new ProductsDownloader.DownloaderParamsStart(
+                txtKey.getText().toString(),
+                new ProductsDownloader.ProgressCallback() {
+                    @Override
+                    public void onStatusChange(CharSequence status) {
+                        lblStatus.setText(status);
+                        if (debugOutput) {
+                            SpannableString s = new SpannableString("    " + status);
+                            s.setSpan(new StyleSpan(Typeface.ITALIC), 0, s.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+                            s.setSpan(new RelativeSizeSpan(0.7f), 0, s.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+                            s.setSpan(new ForegroundColorSpan(0xFFFF0000), 0, s.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+                            s.setSpan(new BackgroundColorSpan(0x20FFA500), 0, s.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+                            appendResultLine(s);
+                        }
+                    }
+                    @Override
+                    public void onDataAdded(CharSequence data) {
+                        appendResultLine(data);
+                    }
+                },
+                new ProductsDownloader.SuccessCallback() {
+                    @Override
+                    public void onSuccess() {
+                        lblStatus.setText("Idle");
+                        toggleDownloaderActive();
+                    }
+                },
+                new ProductsDownloader.CancelCallback() {
+                    @Override
+                    public void onCancel() {
+                        lblStatus.setText("Idle");
+                    }
+                }
+        );
+        productsDownloader.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, params);
+    }
+
+    private void stopDownload() {
+        if (productsDownloader != null) {
+            productsDownloader.cancel(true);
+            productsDownloader = null;
+        }
+    }
+
+    private void toggleDownloaderActive() {
+        downloaderActive = !downloaderActive;
+        btnToggleDownload.setText(downloaderActive ? "Stop" : "Run");
+    }
+
+    private void clearFocus() {
+        lytMain.requestFocus();
+        InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(lytMain.getWindowToken(), 0);
+    }
+
+    private void appendResultLine(CharSequence s) {
+        txtResult.append(s);
+        txtResult.append("\n");
+
+        if (chkScrollContent.isChecked()) {
+            scrlTxtResult.post(new Runnable() {
+                @Override
+                public void run() {
+                    scrlTxtResult.fullScroll(ScrollView.FOCUS_DOWN);
+                }
+            });
+        }
+    }
+}
---
+++ sample_code/android/OpenFoodAPIDemoAndroid/app/src/main/java/com/salathelab/openfoodapidemo/async/ProductsDownloader.java
@@ -0,0 +1,190 @@
+package com.salathelab.openfoodapidemo.async;
+
+import android.os.AsyncTask;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLDecoder;
+
+public class ProductsDownloader extends AsyncTask<ProductsDownloader.DownloaderParamsStart, ProductsDownloader.DownloaderParamsProgress, Void> {
+    public static class DownloaderParamsStart {
+        final String apiKey;
+        final ProgressCallback progressCallback;
+        final SuccessCallback successCallback;
+        final CancelCallback cancelCallback;
+
+        public DownloaderParamsStart(String apiKey, ProgressCallback progressCallback, SuccessCallback successCallback, CancelCallback cancelCallback) {
+            this.apiKey = apiKey;
+            this.progressCallback = progressCallback;
+            this.successCallback = successCallback;
+            this.cancelCallback = cancelCallback;
+        }
+    }
+
+    public static class DownloaderParamsProgress {
+        final String status;
+        final String data;
+
+
+        public DownloaderParamsProgress(String status, String data) {
+            this.status = status;
+            this.data = data;
+        }
+    }
+
+    public interface ProgressCallback {
+        void onStatusChange(CharSequence status);
+        void onDataAdded(CharSequence data);
+    }
+    public interface SuccessCallback {
+        void onSuccess();
+    }
+    public interface CancelCallback {
+        void onCancel();
+    }
+
+
+    final String ORIGIN = "https://www.openfood.ch";
+    final String PRODUCTS_ENDPOINT = "/api/v2/products";
+    long startTaskNanos;
+    DownloaderParamsStart startParams;
+
+    @Override
+    protected Void doInBackground(DownloaderParamsStart... params) {
+        startTaskNanos = System.nanoTime();
+        startParams = params[0];
+        {
+            String displayKey = params[0].apiKey;
+            if (displayKey == null || displayKey.length() == 0) {
+                displayKey = "<No key provided>";
+            }
+            publishProgress(new DownloaderParamsProgress("Got API Key: " + displayKey, null));
+        }
+        String address = ORIGIN + PRODUCTS_ENDPOINT;
+        boolean shouldGetData = true;
+        while (shouldGetData) {
+            long startPageNanos = System.nanoTime();
+            shouldGetData = false;
+            {
+                String displayAddress = address;
+                try {
+                    displayAddress = URLDecoder.decode(address, "UTF-8");
+                } catch (UnsupportedEncodingException e) {
+                }
+                publishProgress(new DownloaderParamsProgress("Connecting to '" + displayAddress + "'...", null));
+            }
+
+            InputStream stream = null;
+            try {
+                URL url = new URL(address);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setConnectTimeout(15000);
+                conn.setRequestMethod("GET");
+                conn.setRequestProperty("Authorization", "Token token=" + params[0].apiKey);
+                try {
+                    stream = conn.getInputStream();
+                } catch (FileNotFoundException e) {
+                    stream = conn.getErrorStream();
+                }
+                publishProgress(new DownloaderParamsProgress("Connected with response code '" + conn.getResponseCode() + "'. Receiving data...", null));
+                BufferedReader reader = new BufferedReader(
+                        new InputStreamReader(stream, "UTF-8"),
+                        1024
+                );
+                String strData = "";
+                {
+                    StringBuilder sb = new StringBuilder();
+                    String line = null;
+                    while ((line = reader.readLine()) != null) {
+                        sb.append(line + "\n");
+                    }
+                    strData = sb.toString();
+                }
+                publishProgress(new DownloaderParamsProgress("Received data in " + (System.nanoTime() - startPageNanos)/1000000000.0f + " seconds. Parsing...", null));
+                try {
+                    JSONObject jsonObj = new JSONObject(strData);
+                    JSONArray products = jsonObj.getJSONArray("data");
+                    int numProducts = products.length();
+                    StringBuilder sb = new StringBuilder();
+                    for (int i = 0; i < numProducts; ++i) {
+                        JSONObject product = products.getJSONObject(i);
+                        int pId = product.getInt("id");
+                        JSONObject productAttrs = product.getJSONObject("attributes");
+                        String pBarcode = productAttrs.getString("barcode");
+                        String pName = productAttrs.getString("name");
+                        if (productAttrs.isNull("name")) {
+                            pName = "";
+                        }
+                        sb.append("id: " + pId + ", barcode: " + pBarcode + ", name: \"" + pName + "\"\n");
+                    }
+                    publishProgress(new DownloaderParamsProgress("Parsed " + numProducts + " products", sb.toString()));
+                    JSONObject links = jsonObj.getJSONObject("links");
+                    String nextAddress = links.getString("next");
+                    if (nextAddress != null) {
+                        shouldGetData = true;
+                        address = nextAddress;
+                        publishProgress(new DownloaderParamsProgress("Found link to next page of data", null));
+                    }
+                } catch (JSONException e) {
+                    publishProgress(new DownloaderParamsProgress("Error parsing JSON", strData));
+                }
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (stream != null) {
+                    try {
+                        stream.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    @Override
+    protected void onProgressUpdate(DownloaderParamsProgress... values) {
+        if (values[0].status != null) {
+            startParams.progressCallback.onStatusChange(values[0].status);
+        }
+        if (values[0].data != null) {
+            startParams.progressCallback.onDataAdded(values[0].data);
+        }
+    }
+
+    @Override
+    protected void onCancelled() {
+        super.onCancelled();
+        onProgressUpdate(new DownloaderParamsProgress(
+                "Cancelled after " + (System.nanoTime() - startTaskNanos)/1000000000.0f + " seconds",
+                null
+        ));
+        if (startParams.cancelCallback != null) {
+            startParams.cancelCallback.onCancel();
+        }
+    }
+
+    @Override
+    protected void onPostExecute(Void result) {
+        onProgressUpdate(new DownloaderParamsProgress(
+                "Completed after " + (System.nanoTime() - startTaskNanos)/1000000000.0f + " seconds",
+                null
+        ));
+        if (startParams.successCallback != null) {
+            startParams.successCallback.onSuccess();
+        }
+    }
+}
